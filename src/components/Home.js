f0326cb459c6a88e25f1157f14228a7ee96be4ea
Esteban297 <estebancristancho9@gmail.com>
2023-04-17 20:11:53 -0500

    ajustes home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import logo from './img/img.png'; // Ruta relativa a la imagen
+
 
 const Home = () => {
     return (
         <div className="home-container">
             <div className="header">
+                <img src={logo} alt="Logo de la empresa" style={{ width: '10%', height: '50%' }} />
                 <Link to="/home" className="home-link">Home</Link>
+                <Link to="/points" className="profile-link">Points</Link>
                 <Link to="/help" className="help-link">Help</Link>
                 <Link to="/profile" className="profile-link">Profile</Link>
             </div>
